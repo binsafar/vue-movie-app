b14c07df661a598f8296f8ec82bb02c23bef570e
binsafar <yokubibnsafar@gmail.com>
2023-02-13 23:08:38 +0500

    fixed: content;

--- src/pages/Content.vue
+++ src/pages/Content.vue
@@ -1,26 +1,16 @@
 <template>
   <CommentsCom :load-more="loadMore" :comments='comments' :load-function="load"/>
+  <RecommendedCourse/>
 </template>
 
 <script setup>
 import CommentsCom from "../components/Content/Comments/CommentsCom.vue";
 import {ref} from "vue"
+import RecommendedCourse from "../components/Content/RecommendedCourse/RecommendedCourse.vue";
 
 function load() {
-  if (loadMore.value === true) {
-    comments.value.comments.push({
-      id: 5,
-      name: 'KKomol',
-      date: '2 дня назад',
-      text: 'Нет ребят я конечно все понимаю таких фильмов с похожим сюжетом пруд пруди, \n' +
-          'но думаю именно этот стоит посмотреть :)',
-      avatar: '',
-      reply: [
-        {id: 6, date: '1 день назад', text: 'Нет ребят я конечно все понимаю таких фильмов'}
-      ]
-    },)
-    loadMore.value = false
-  }
+  comments.value.comments.push(comments.value.comments[0])
+  loadMore.value = false
 }
 
 const loadMore = ref(true)
