f3f745866ff572191a9b8819c63d0403dc78e09b
binsafar <yokubibnsafar@gmail.com>
2023-02-13 23:00:35 +0500

    added: comments page

--- src/components/Content/Comments/CommentsCom.vue
+++ src/components/Content/Comments/CommentsCom.vue
@@ -1,14 +1,11 @@
 <template>
-  <button @click="loadFunction">loaaaaaad</button>
-  <main>
+  <main class="comment-main">
     <div class="comment-info">
       <p class="comment-amount">{{ comments.amount }} Комментария</p>
-
       <div class="comment-input">
         <img src="../../../assets/img/user_avatar.png" alt="user logo">
-        <input type="text" placeholder="Оставить отзыв...">
+        <input v-model="commentInput.input" type="text" placeholder="Оставить отзыв...">
       </div>
-
     </div>
 
     <div class="comments">
@@ -17,27 +14,34 @@
         <div class="comment-itself">
           <img src="../../../assets/img/comment_avatar.png" alt="user avatar">
           <div class="comment-info">
-            <p>{{ comment.date }}</p>
-            <p>{{ comment.text }}</p>
+            <p>{{ comment.name }}</p>
+            <p class="grey-text">{{ comment.date }}</p>
+            <p class="comment-desc">{{ comment.text }}</p>
           </div>
         </div>
         <!--comment reply-->
         <div v-for="reply in comment.reply" class="comment-reply">
           <img src="../../../assets/img/admin_logo.png" alt="admin avatar">
           <div class="comment-r-info">
-            <p>TopBreyns - Поддержка</p>
-            <p>{{ reply.date }}</p>
-            <p>{{ reply.text }}</p>
+            <p class="grey-background">TopBreyns - Поддержка</p>
+            <p class="grey-text">{{ reply.date }}</p>
+            <p class="comment-desc">{{ reply.text }}</p>
           </div>
         </div>
       </section>
     </div>
+
+    <button v-if="loadMore" class="load-more" @click="loadFunction">Показать ещё</button>
   </main>
 </template>
 
 <script setup>
+import {ref} from "vue"
+
+const commentInput = ref({input: ''})
 defineProps({
   comments: Object,
-  loadFunction: Function
+  loadFunction: Function,
+  loadMore: Boolean
 })
 </script>
--- src/pages/Content.vue
+++ src/pages/Content.vue
@@ -1,16 +1,16 @@
 <template>
-  <CommentsCom :comments='comments' :load-function="load"/>
+  <CommentsCom :load-more="loadMore" :comments='comments' :load-function="load"/>
 </template>
 
 <script setup>
 import CommentsCom from "../components/Content/Comments/CommentsCom.vue";
-
-let loadMore = false
+import {ref} from "vue"
 
 function load() {
-  if (loadMore === false) {
-    comments.comments.push({
+  if (loadMore.value === true) {
+    comments.value.comments.push({
       id: 5,
+      name: 'KKomol',
       date: '2 дня назад',
       text: 'Нет ребят я конечно все понимаю таких фильмов с похожим сюжетом пруд пруди, \n' +
           'но думаю именно этот стоит посмотреть :)',
@@ -19,16 +19,17 @@ function load() {
         {id: 6, date: '1 день назад', text: 'Нет ребят я конечно все понимаю таких фильмов'}
       ]
     },)
-    loadMore = true
+    loadMore.value = false
   }
-  console.log(comments)
 }
 
-let comments = {
+const loadMore = ref(true)
+const comments = ref({
   amount: 32,
   comments: [
     {
       id: 1,
+      name: 'KKomol',
       date: '2 дня назад',
       text: 'Нет ребят я конечно все понимаю таких фильмов с похожим сюжетом пруд пруди, \n' +
           'но думаю именно этот стоит посмотреть :)',
@@ -39,6 +40,7 @@ let comments = {
     },
     {
       id: 2,
+      name: 'KKomol',
       date: '2 дня назад',
       text: 'Нет ребят я конечно все понимаю таких фильмов с похожим сюжетом пруд пруди, \n' +
           'но думаю именно этот стоит посмотреть :)',
@@ -46,6 +48,7 @@ let comments = {
     },
     {
       id: 3,
+      name: 'KKomol',
       date: '2 дня назад',
       text: 'Нет ребят я конечно все понимаю таких фильмов с похожим сюжетом пруд пруди, \n' +
           'но думаю именно этот стоит посмотреть :)',
@@ -56,11 +59,12 @@ let comments = {
     },
     {
       id: 4,
+      name: 'KKomol',
       date: '2 дня назад',
       text: 'Нет ребят я конечно все понимаю таких фильмов с похожим сюжетом пруд пруди, \n' +
           'но думаю именно этот стоит посмотреть :)',
       avatar: '',
     },
   ]
-}
+})
 </script>
