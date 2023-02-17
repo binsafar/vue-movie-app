e1da19698d82ac3cbfc4706c913aa33ec7fbb021
binsafar <yokubibnsafar@gmail.com>
2023-02-17 19:57:57 +0500

    scores fixed

--- src/components/Content/VideoLesson/VideoLesson.vue
+++ src/components/Content/VideoLesson/VideoLesson.vue
@@ -11,7 +11,7 @@
           <p class="left-video-desc grey-text">{{ lessonDb.lessonDesc }}</p>
         </div>
         <!--mobile content-->
-        <div class="mobile-content">
+        <div :class="showModal === true ? 'show-modal mobile-content':'mobile-content'">
           <button>
             <img src="../../../assets/icon/lesson_icons/play_btn.svg" alt="play">
             След. Урок
@@ -24,7 +24,7 @@
             <img src="../../../assets/icon/lesson_icons/comment_btn.svg" alt="comment">
             Коментарии
           </button>
-          <button>
+          <button @click="showModal !== showModal">
             <img src="../../../assets/icon/lesson_icons/show_lesson_btn.svg" alt="show lessons modal">
             Все уроки
           </button>
@@ -53,6 +53,38 @@
           <p v-if="isActive === lesson.id" class="r-lessons-desc grey-text">{{ lesson.desc }}</p>
         </div>
       </div>
+      <!--lesson cards-->
+      <div class="right-card-info">
+        <div class="right-card author">
+          <div class="rc-wrapper">
+            <img class="teacher-img" :src="lessonDb.cardsInfo.author.img" alt="avatar">
+            <p>{{ lessonDb.cardsInfo.author.name }}</p>
+          </div>
+        </div>
+
+        <div class="right-card lesson-score">
+          <div class="rc-wrapper">
+            <p class="ls-score-title">{{ lessonDb.cardsInfo.lessonScore.completed }}
+              <span class="grey-text">/ {{ lessonDb.cardsInfo.lessonScore.total }}</span>
+            </p>
+            <p>Уроков завершено</p>
+          </div>
+        </div>
+
+        <div class="right-card lessons">
+          <div class="rc-wrapper">
+            <p class="lesson-number">{{ lessonDb.cardsInfo.lessons.amount }}</p>
+            <p>Урока за сегодня</p>
+          </div>
+        </div>
+
+        <div class="right-card student">
+          <div class="rc-wrapper">
+            <img :src="lessonDb.cardsInfo.student.img" alt="">
+            <p>{{ lessonDb.cardsInfo.student.name }}</p>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -62,5 +94,6 @@ import lessonDb from "../../../service/db/lessonDb.js";
 import {ref} from "vue"
 
 const isActive = ref(1)
+const showModal = ref(true);
 const setActive = (id) => isActive.value = id
 </script>
